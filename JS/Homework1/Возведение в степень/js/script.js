9728a9ee782580304020eacf821cdf01b0892b1a
Karas83 <sharoshkin1996@gmail.com>
2016-03-04 03:06:09 +0200

    js

---
+++ JS/Homework1/Возведение в степень/js/script.js
@@ -0,0 +1,7 @@
+﻿	
+function pow(a, b){
+	a=prompt ('Введите основание:');
+	b=prompt ('Введите степень:');
+	console.log('Результат=',Math.pow(a,b));
+}
+pow()
---
+++ JS/Homework1/Цыкл/js/script.js
@@ -0,0 +1,14 @@
+﻿var arr = [];
+var name;
+var finallName;
+for (i = 0; i<5; i++) {
+	arr.push(prompt ("Введите имя для массива"));
+}
+name=prompt('Введите имя');
+finallName='Ошибка входа';
+for (i = 0; i<5; i++) {
+	if (name === arr[i]){
+			finallName=(name+', вы успешно вошли')
+	}
+}
+alert(finallName);
