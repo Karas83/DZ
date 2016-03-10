9db6f11be43ecb4bbc7603221a70c81e04c6ecb3
Karas83 <sharoshkin1996@gmail.com>
2016-03-10 21:14:14 +0200

    hm

--- JS/Homework1/Возведение в степень/js/script.js
+++ JS/Homework1/Возведение в степень/js/script.js
@@ -2,6 +2,6 @@
 function pow(a, b){
 	a=prompt ('Введите основание:');
 	b=prompt ('Введите степень:');
-	console.log('Результат=',Math.pow(a,b));
+	console.log('Результат=',Math.exp(b*Math.log(a)));
 }
 pow()
