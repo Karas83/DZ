4ca28913304d27a20af1711549cce249ac33876a
Karas83 <sharoshkin1996@gmail.com>
2016-03-18 21:45:13 +0200

    hm4

---
+++ JS/Homework4/Form/js/script.js
@@ -0,0 +1,20 @@
+﻿$(function() {
+
+  var $input = $('input');
+  var $help = $('button');
+  var $span = $('span');
+  
+  $input.hover (
+	function(){
+		var $span = $(this).siblings('.span');
+		$span.fadeTo("slow", 1);
+	},
+	function(){
+		var $span = $(this).siblings('.span');
+		$span.fadeTo("slow", 0);
+	}
+	);
+	$help.on ('click', function(){
+		$span.fadeTo("slow", 1);
+	});
+});
---
+++ JS/Homework4/Tabs/js/script.js
@@ -0,0 +1,52 @@
+﻿$(function() {
+
+  var $tabs1 = $('.a1');
+  var $tabs2 = $('.a2');
+  var $tabs3 = $('.a3');
+  var $div1 = $('.div1');
+  var $div2 = $('.div2');
+  var $div3 = $('.div3');
+  
+  $tabs1.on ('click', function(e){
+	e.preventDefault();
+	$div1.css('display', 'block');
+	$div2.css('display', 'none');
+	$div3.css('display', 'none');
+		$tabs2.css('background-color', 'rgba(0,0,0,0.1)');
+		$tabs2.css('border', 'transparent');
+		$tabs1.css('background-color', 'rgba(0,0,0,0.3)');
+		$tabs1.css('border', '1px solid black');
+		$tabs3.css('background-color', 'rgba(0,0,0,0.1)');
+		$tabs3.css('border', 'transparent');
+  });
+  
+   $tabs2.on ('click', function(e){
+	e.preventDefault();
+	$div1.css('display', 'none');
+	$div2.css('display', 'block');
+	$div3.css('display', 'none');
+	$tabs2.css('background-color', 'rgba(0,0,0,0.3)');
+	$tabs2.css('border', '1px solid black');
+	$tabs1.css('background-color', 'rgba(0,0,0,0.1)');
+	$tabs1.css('border', 'transparent');
+	$tabs3.css('background-color', 'rgba(0,0,0,0.1)');
+	$tabs3.css('border', 'transparent');
+  });
+  
+   $tabs3.on ('click', function(e){
+	e.preventDefault();
+	$div1.css('display', 'none');
+	$div2.css('display', 'none');
+	$div3.css('display', 'block');
+		$tabs2.css('background-color', 'rgba(0,0,0,0.1)');
+		$tabs2.css('border', 'transparent');
+		$tabs1.css('background-color', 'rgba(0,0,0,0.1)');
+		$tabs1.css('border', 'transparent');
+		$tabs3.css('background-color', 'rgba(0,0,0,0.3)');
+		$tabs3.css('border', '1px solid black');
+  });
+
+
+	
+	
+});
