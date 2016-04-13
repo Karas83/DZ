67a6a48b31913e3a65e6694c40013fe3cceefde7
Karas83 <sharoshkin1996@gmail.com>
2016-04-13 21:30:54 +0300

    hm

---
+++ JS/Homework8/Google/js/script.js
@@ -0,0 +1,33 @@
+﻿
+$(document).ready(function(){
+    
+    $('#f').submit(function(){
+    
+        var url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&rsz=large&q='+ encodeURIComponent($('#q').val()) + '&callback=GoogleCallback&context=?';
+        
+        $.ajax({
+           url: url,
+           dataType : "jsonp",
+           
+           success: function (data, textStatus) {
+				var ul = document.createElement("ul");
+                
+                $.each(data.results,
+    				function(i, val) {
+    					var li = document.createElement("li");
+    					li.innerHTML = '<a href="'+val.url+'"target="_blank">'+val.title+'</a><p class="url">'+val.url+'</p><p class="content">'+val.content+'</p>';        						
+    					ul.appendChild(li);
+    					}
+    				);
+				$('#results').html(ul);
+		    }				
+		});
+        return false;
+    });
+    
+});
+
+function GoogleCallback (func, data) {
+    window[func](data);
+}
+    
---
+++ JS/Homework8/Proto/js/script.js
@@ -0,0 +1,70 @@
+﻿function Human() {
+	this.name = 'Ivan';
+	this.age = 30;
+	this.kind = 'male';
+	this.growth = 180;
+	this.weight = 90;
+};
+
+function Worker() {
+	this.work = function () {	
+		placeWork = 'Go it';
+		salary = 5000;
+	};
+};
+function Worker1() {
+	this.work = function () {	
+		placeWork = 'Стройка';
+		salary = 3000;
+	};
+};
+function Worker2() {
+	this.work = function () {	
+		placeWork = 'ЖЭК';
+		salary = 5000;
+	};
+};
+
+function Student() {
+	this.watchSerials = function () {	
+		placeStudy = 'Khai';
+		grants = 900;
+	};
+};
+function Student1() {
+	this.watchSerials = function () {	
+		placeStudy = 'Khadi';
+		grants = 900;
+	};
+};
+function Student2() {
+	this.watchSerials = function () {	
+		placeStudy = 'Infiz';
+		grants = 900;
+	};
+};
+
+Worker.prototype = new Human;
+var newWorker = new Worker();
+Worker1.prototype = new Human;
+var newWorker1 = new Worker();
+Worker1.prototype = new Human;
+var newWorker2 = new Worker();
+
+Student.prototype = new Human;
+var newStudent = new Student();
+Student1.prototype = new Human;
+var newStudent1 = new Student();
+Student2.prototype = new Human;
+var newStudent2 = new Student();
+
+console.log(newWorker.work);
+console.log(newWorker1.age);
+console.log(newWorker2.kind);
+
+console.log(newStudent.watchSerials);
+console.log(newStudent1.weight);
+console.log(newStudent2.name);
+
+
+
