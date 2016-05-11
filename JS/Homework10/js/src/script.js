eaac5bc22ee7295cbc9f51481b1da5a0078ed99f
Karas83 <sharoshkin1996@gmail.com>
2016-05-11 21:28:14 +0300

    dz

---
+++ JS/Homework10/js/src/script.js
@@ -0,0 +1,20 @@
+$(function() {
+    $('.jcarousel').jcarousel({
+        // Core configuration goes here
+		wrap: 'both',
+		animation: {
+        duration: 800,
+        easing:   'linear',
+        complete: function() {
+        }},
+		
+    });
+	
+    $('.jcarousel-prev').jcarouselControl({
+        target: '+=1'
+    });
+
+    $('.jcarousel-next').jcarouselControl({
+        target: '-=1'
+    });
+});
